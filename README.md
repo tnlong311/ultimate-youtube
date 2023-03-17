144c6e3a56b066880920797132209f449e2aaa9b
ngoclong-fulbright <69509154+ngoclong-fulbright@users.noreply.github.com>
2023-03-17 19:55:57 +0700

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-Webpack 5 install:
-```
-npm install --save-dev fs assert https-browserify os os-browserify stream-browserify stream-http
-```
--- src/consts.js
+++
@@ -1,9 +0,0 @@
-const embedLink = "https://www.youtube.com/embed/"
-
-const getEmbedLink = (id, time) => {
-  return embedLink + id + "?start=" + time
-}
-
-const pageNumRegex = /^[0-9]*$/
-
-export {getEmbedLink, pageNumRegex}
